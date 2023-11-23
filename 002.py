a6e8df46f2be92d9eab5e701ad0a363fb39d1b75
oleks-kondrashov <kondrashov.alx@gmail.com>
2023-11-23 13:27:10 +0200

    002 solution

--- 002.py
+++ 002.py
@@ -1,9 +1,17 @@
-index = 1
-fib = [1, 2]
-limit = 4000000
+#https://projecteuler.net/problem=2
 
-while fib[index] + fib[index - 1] < limit:
-    fib.append(fib[index] + fib[index - 1])
-    index = index + 1
+def calc():
+    index = 1
+    fib = [1, 2]
+    limit = 4000000
 
-print(sum([fib[x] for x in range(0, len(fib)) if (fib[x] % 2) == 0]))
+    while fib[index] + fib[index - 1] < limit:
+        fib.append(fib[index] + fib[index - 1])
+        index = index + 1
+
+    res = sum([fib[x] for x in range(0, len(fib)) if (fib[x] % 2) == 0])
+    return res
+
+
+if __name__ == "__main__":
+    print(calc())
